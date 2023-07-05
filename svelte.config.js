ff3b640eec20b2d17d8b6dd430d8af46d9d60be2
CarlosGDCJ <32521936+CarlosGDCJ@users.noreply.github.com>
2023-07-05 13:34:38 -0300

    Change actions workflow

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -15,7 +17,10 @@ const config = {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter()
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH
+        }
     }
 };
 
