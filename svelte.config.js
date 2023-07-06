35c6ff8025949fad8eaa0bfc0e2b678b905ac14d
CarlosGDCJ <32521936+CarlosGDCJ@users.noreply.github.com>
2023-07-06 20:17:01 -0300

    Favicon nightmare continues

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,8 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter(),
         paths: {
-            base: dev ? '' : process.env.BASE_PATH
+            base: dev ? '' : process.env.BASE_PATH,
+            assets: dev ? '' : process.env.ASSETS_PATH
         }
     }
 };
